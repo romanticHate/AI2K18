783f5594154b363a99e174935bf38aa0294ada99
RomanticHate <aivp032381@gmail.com>
2018-10-19 09:01:00 -0600

    FireBase A1

--- FireBaseA1/app/src/main/java/com/example/romantichate/firebasea1/MainActivity.java
+++ FireBaseA1/app/src/main/java/com/example/romantichate/firebasea1/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.romantichate.firebasea1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 public class MainActivity extends AppCompatActivity {
-
+private Button mSendData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSendData = (Button) findViewById(R.id.sendData);
     }
 }
