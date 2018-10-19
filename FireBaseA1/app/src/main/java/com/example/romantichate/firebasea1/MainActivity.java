f6cdd6ab37890565537a8e0f55578441187d24c1
RomanticHate <aivp032381@gmail.com>
2018-10-19 12:54:36 -0600

    FireBase A1

--- FireBaseA1/app/src/main/java/com/example/romantichate/firebasea1/MainActivity.java
+++ FireBaseA1/app/src/main/java/com/example/romantichate/firebasea1/MainActivity.java
@@ -1,19 +1,51 @@
 package com.example.romantichate.firebasea1;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.IgnoreExtraProperties;
 
 public class MainActivity extends AppCompatActivity {
-private Button mSendData;
+   //FireBase
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
+    //Controles
+    private Button mSendData;
+    private EditText editText;
+    private EditText editText2;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //FireBase
+        //mAuth = FirebaseAuth.getInstance();
+        //FirebaseUser currentUser = mAuth.getCurrentUser();
+        //mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        //Controles
         mSendData = (Button) findViewById(R.id.sendData);
+        editText = (EditText) findViewById(R.id.editText);
+        editText2 = (EditText) findViewById(R.id.editText2);
     }
+
+
 }
