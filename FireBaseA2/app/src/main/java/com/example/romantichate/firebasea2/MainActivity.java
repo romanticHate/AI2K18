bdd3b0dcc2fe845c79fc89e09754cb005c772354
RomanticHate <aivp032381@gmail.com>
2018-10-19 14:52:06 -0600

    FireBase A2

---
+++ FireBaseA2/app/src/main/java/com/example/romantichate/firebasea2/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.romantichate.firebasea2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Add toolbar
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle("Firebase Demo");
+        setSupportActionBar(toolbar);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
