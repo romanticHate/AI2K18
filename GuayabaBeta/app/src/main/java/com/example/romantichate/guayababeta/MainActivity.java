10d1b85da9605eeaf8cf236e6cc58245ae6a455f
RomanticHate <aivp032381@gmail.com>
2018-10-20 18:29:08 -0600

    Guayaba (beta)

--- GuayabaBeta/app/src/main/java/com/example/romantichate/guayababeta/MainActivity.java
+++ GuayabaBeta/app/src/main/java/com/example/romantichate/guayababeta/MainActivity.java
@@ -4,17 +4,22 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -23,12 +28,15 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private EditText editTextName;
-    private Button buttonAdd;
+    private TextView textViewMsg;
+    private Button buttonAdd,buttonLoad;
     private Spinner spinnerGender;
     //region Code: Basura
     //DatabaseReference databaseUsers;
     //endregion
     FirebaseFirestore mFireStore;
+
+    private static final String FIRE_LOG = "Fire_Log";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,9 +47,29 @@ public class MainActivity extends AppCompatActivity {
         //region Code: Basura
         //databaseUsers = FirebaseDatabase.getInstance().getReference("users");
         //endregion
+        textViewMsg = findViewById(R.id.textViewMsg);
         editTextName = findViewById(R.id.editTextName);
         buttonAdd = findViewById(R.id.buttonAdd);
+        buttonLoad = findViewById(R.id.buttonLoad);
         spinnerGender = findViewById(R.id.spinnerGender);
+
+        buttonLoad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mFireStore.collection("users").document("xyz").get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if(task.isSuccessful()){
+                            DocumentSnapshot documentSnapshot = task.getResult();
+                            String name = documentSnapshot.getString("name");
+                            textViewMsg.setText("Welcome guayaba user:" + name);
+                        }else{
+                            Log.d(FIRE_LOG,"Error: "+task.getException().getMessage());
+                        }
+                    }
+                });
+            }
+        });
 
         buttonAdd.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,11 +85,20 @@ public class MainActivity extends AppCompatActivity {
     private void addDataToFireBase() {
         String name = editTextName.getText().toString().trim();
         String gender = spinnerGender.getSelectedItem().toString();
-
+        //User user = new User(name,gender);
         Map<String, String> userMap = new HashMap<>();
         userMap.put("name", name);
         userMap.put("gender", gender);
 
+        mFireStore.collection("users").document("xyz").set(userMap).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+                Toast.makeText(MainActivity.this,"Error: " + e.getMessage(),Toast.LENGTH_LONG).show();
+            }
+        });
+
+        /*
         mFireStore.collection("users").add(userMap).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
             @Override
             public void onSuccess(DocumentReference documentReference) {
@@ -75,6 +112,7 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this,"Error: " + error,Toast.LENGTH_LONG).show();
             }
         });
+        */
     }
     //region  Code: basura
 /*
